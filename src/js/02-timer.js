8a0512bb1d18e52c20fda86cfa531f58ec21c8f7
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-03-11 15:48:13 +0100

    added code task 2 and try task3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,20 +9,59 @@ import Notiflix from 'notiflix';
 //Biblioteka czeka na jej inicjalizację w elemencie input[type="text"], dlatego dodaliśmy do HTML input#datetime-picker.
 
 const startInput = document.querySelector('input#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const days = document.querySelector('.value[data-days]');
+const hours = document.querySelector('.value[data-hours]');
+const minutes = document.querySelector('.value[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
 
+const currentDate = Date.now();
 
+let dateElClock = null;
 
+startBtn.disabled = true;
 
 //from homework
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        
+        dateElClock = selectedDates[0];
+        if (dateElClock > currentDate) {
+            startBtn.disabled = false;
+        }
+        else { Notiflix.Notify.failure("Please choose a date in the future"); }
+    }
 };
+    const flatpickrEl = new flatpickr(startInput, options);
+    startBtn.addEventListener('click', updateClockRun);
+
+    function updateClockRun(){
+
+        const intervalId = setInterval(() => {
+            const currentDate = Date.now();
+            const deltaTime = dateElClock - currentDate;
+            days.textContent = convertMs(deltaTime).days;
+            hours.textContent = convertMs(deltaTime).hours;
+            minutes.textContent = convertMs(deltaTime).minutes;
+            seconds.textContent = convertMs(deltaTime).seconds;
+            
+            startBtn.disabled = true;
+            flatpickrEl.input.setAttribute("disabled", "disabled")
+            if (deltaTime < 1000) {
+                clearInterval(intervalId);
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+                startBtn.disabled = false;
+            }
+        }, 1000);
+}
+
 
 
 
@@ -34,13 +73,20 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = pad(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = pad(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
-  return { days, hours, minutes, seconds };
+  return { days,hours,minutes,seconds};
+}
+
+//Funkcja pad() służy do "wypełnienia" wartości numerowej z przodu zadaną liczbą znaków.Funkcja przyjmuje argument value, który jest liczbą lub wartością, która może być rzutowana na typ string. Następnie funkcja konwertuje ten argument na typ string za pomocą String(value) i wykorzytsuje metode padStart() z liczbą znaków i wypełniaczem 
+
+
+function pad(value) {
+    return String(value).padStart(2,"0")
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,16 @@
+import Notiflix from "notiflix";
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmitForm);
+function onSubmitForm(e) {
+  e.preventDefault();
+  let { delay, step, amount } = e.currentTarget;
+  
+}
+
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
