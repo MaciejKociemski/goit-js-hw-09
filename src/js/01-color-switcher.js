893301655b0b779cc6246a21c82bf9e599f082bf
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-03-11 10:01:58 +0100

    make code to task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+//from content homework
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+const startButton = document.querySelector("[data-start");
+const stopButton = document.querySelector("[data-stop");
+const bodyColor = document.querySelector('body');
+
+let clockId = null;
+startButton.disabled = false;
+startButton.addEventListener("click)", () => {
+    clockId = setInterval(() => {
+
+        startButton.disabled = true;
+        bodyColor.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+  
+stopButton.addEventListener("click", () => {
+    clearInterval(clockId);
+})
+
 
