abac92023ac0b0695c33f92809df36c1ed31b4c8
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-03-11 09:45:11 +0100

    added code in task 2 from homework

---
+++ src/js/02-timer.js
@@ -0,0 +1,46 @@
+// Opisany w dokumentacji
+import flatpickr from "flatpickr";
+// Dodatkowy import stylów
+import "flatpickr/dist/flatpickr.min.css";
+
+//library replacement window.alert()
+import Notiflix from 'notiflix';
+
+//Biblioteka czeka na jej inicjalizację w elemencie input[type="text"], dlatego dodaliśmy do HTML input#datetime-picker.
+
+const startInput = document.querySelector('input#datetime-picker');
+
+
+
+
+//from homework
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
