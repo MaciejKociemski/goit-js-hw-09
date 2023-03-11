1c804b69d58ef9f0f9c068a4a5aad6e6e740a2ac
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-03-11 11:20:43 +0100

    added code task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,25 +1,23 @@
 //from content homework
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
-
-const startButton = document.querySelector("[data-start");
-const stopButton = document.querySelector("[data-stop");
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
 const bodyColor = document.querySelector('body');
 
-let clockId = null;
-startButton.disabled = false;
-startButton.addEventListener("click)", () => {
-    clockId = setInterval(() => {
-
-        startButton.disabled = true;
-        bodyColor.style.backgroundColor = getRandomHexColor();
-    }, 1000);
+let timerId = null;
+startBtn.disabled =false;
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+      startBtn.disabled = true;
+      
+    bodyColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
 });
-  
-stopButton.addEventListener("click", () => {
-    clearInterval(clockId);
-})
 
-
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  startBtn.disabled =false;
+});
